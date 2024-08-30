389425d90081eb851ff6e5ba3e0ecc06f9101316
thonghp <thonghoangpham123@gmail.com>
2024-08-30 18:04:20 +0700

    hero section strapi

---
+++ backend-v2/types/generated/components.d.ts
@@ -0,0 +1,65 @@
+import type { Struct, Schema } from '@strapi/strapi';
+
+export interface LayoutHeroSection extends Struct.ComponentSchema {
+  collectionName: 'components_layout_hero_sections';
+  info: {
+    displayName: 'Hero Section';
+    description: '';
+  };
+  attributes: {
+    heading: Schema.Attribute.String;
+    subHeading: Schema.Attribute.Text;
+    image: Schema.Attribute.Media<'images'>;
+    link: Schema.Attribute.Component<'components.link', false>;
+  };
+}
+
+export interface LayoutFeaturesSection extends Struct.ComponentSchema {
+  collectionName: 'components_layout_features_sections';
+  info: {
+    displayName: 'Features Section';
+    description: '';
+  };
+  attributes: {
+    title: Schema.Attribute.String;
+    description: Schema.Attribute.Text;
+    feature: Schema.Attribute.Component<'components.feature', true>;
+  };
+}
+
+export interface ComponentsLink extends Struct.ComponentSchema {
+  collectionName: 'components_components_links';
+  info: {
+    displayName: 'Link';
+  };
+  attributes: {
+    url: Schema.Attribute.String;
+    text: Schema.Attribute.String;
+    isExternal: Schema.Attribute.Boolean & Schema.Attribute.DefaultTo<false>;
+  };
+}
+
+export interface ComponentsFeature extends Struct.ComponentSchema {
+  collectionName: 'components_components_features';
+  info: {
+    displayName: 'Feature';
+  };
+  attributes: {
+    heading: Schema.Attribute.String;
+    subHeading: Schema.Attribute.Text;
+    icon: Schema.Attribute.Enumeration<
+      ['CLOCK_ICON', 'CHECK_ICON', 'CLOUD_ICON']
+    >;
+  };
+}
+
+declare module '@strapi/strapi' {
+  export module Public {
+    export interface ComponentSchemas {
+      'layout.hero-section': LayoutHeroSection;
+      'layout.features-section': LayoutFeaturesSection;
+      'components.link': ComponentsLink;
+      'components.feature': ComponentsFeature;
+    }
+  }
+}
